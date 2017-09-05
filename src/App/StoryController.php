73d8540a5d123a1ac7998d75b63566dbf0fe50f8
shawn sandy <shawnsandy04@gmail.com>
2017-09-05 14:52:56 -0400

    Component : add story controller

---
+++ src/App/StoryController.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: shawnsandy
+ * Date: 9/5/17
+ * Time: 2:24 PM
+ */
+
+namespace ShawnSandy\Backstory\App;
+
+
+use Illuminate\Routing\Controller;
+
+class StoryController extends Controller
+{
+
+    public function store() {
+
+    }
+
+    public function update() {
+
+    }
+
+    public function destroy() {
+
+    }
+}
--- src/routes.php
+++ src/routes.php
@@ -5,3 +5,7 @@
  * Date: 10/27/16
  * Time: 12:58 PM
  */
+
+
+Route::resource('stories', "\ShawnSandy\Backstory\App\StoryController",
+    ['only' => ["store", "update", "destroy"]]);
