6670aecf33b87ca64f294b2565647a940e53fc7b
shawnsandy04 <shawnsandy04@gmail.com>
2017-09-06 20:02:00 -0400

    Update : add user realtionship method author()

--- src/App/Story.php
+++ src/App/Story.php
@@ -2,9 +2,14 @@
 
 namespace ShawnSandy\Backstory\App;
 
+use App\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Story extends Model
 {
+
+    public function author() {
+        return $this->belongsTo(User::class);
+    }
 
 }
