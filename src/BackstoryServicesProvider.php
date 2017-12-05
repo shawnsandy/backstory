f3b8e831cd1b15e1ec404f80f9bd4a25188073af
Shawn Sandy <shawnsandy04@gmail.com>
2017-12-05 08:10:21 -0500

    packages : add choices.js

--- src/BackstoryServicesProvider.php
+++ src/BackstoryServicesProvider.php
@@ -10,183 +10,185 @@ use Spatie\Html\HtmlServiceProvider;
 
 /**
 Class Provider
- * @package ShawnSandy\Backstory
- */
+* @package ShawnSandy\Backstory
+*/
 class BackstoryServicesProvider extends ServiceProvider
 {
 
 
-	/**
-	* Perform post-registration booting of services.
-		     *
-		     * @return void
-		     */
-		    public function boot()
-		    {
+/**
+* Perform post-registration booting of services.
+*
+* @return void
+*/
+public function boot()
+{
 
 
 
 
-		/**
-		* Package views
-				         */
-				        $this->loadViewsFrom(__DIR__ . '/resources/views', 'backstory');
-		$this->publishes(
-				            [
-				                __DIR__ . '/resources/views' => resource_path('views/vendor/backstory'),
-				            ], 'backstory-views'
-				        );
+/**
+* Package views
+    */
+$this->loadViewsFrom(__DIR__ . '/resources/views', 'backstory');
+$this->publishes(
+    [
+        __DIR__ . '/resources/views' => resource_path('views/vendor/backstory'),
+    ], 'backstory-views'
+);
 
 
 
-		/**
-		* Package assets
-				         */
-				        $this->publishes(
-				            [
+/**
+* Package assets
+    */
+$this->publishes(
+    [
 
-				                __DIR__.'/resources/assets/sass' => public_path('assets/backstory'),
-				                __DIR__.'/../node_modules/ckeditor' => public_path('assets/backstory/ckeditor/'),
-				            ], 'backstory-assets'
-				        );
+        __DIR__.'/resources/assets/sass' => public_path('assets/backstory'),
+        __DIR__.'/../node_modules/ckeditor' => public_path('assets/backstory/ckeditor/'),
+        __DIR__.'/../node_modules/choices.js/assets/scripts/dist' => public_path('assets/backstory/choices/'),
+        __DIR__.'/../node_modules/choices.js/assets/styles/css' => public_path('assets/backstory/choices/'),
+    ], 'backstory-assets'
+);
 
 
 
 
-		/**
-		* Package resources to resources
-				         */
-				        $this->publishes(
-				            [
-				                __DIR__.'/resources/assets/' => resource_path('assets/backstory'),
-				            ], 'backstory-resources'
-				        );
+/**
+* Package resources to resources
+    */
+$this->publishes(
+    [
+        __DIR__.'/resources/assets/' => resource_path('assets/backstory'),
+    ], 'backstory-resources'
+);
 
 
 
-		/**
-		* Package config
-				         */
-				        $this->publishes(
-				            [__DIR__ . '/config/config.php' => config_path('backstory.php')],
-				            'backstory-config'
-		                );
+/**
+* Package config
+    */
+$this->publishes(
+    [__DIR__ . '/config/config.php' => config_path('backstory.php')],
+    'backstory-config'
+);
 
 
-		$this->publishes([
-		            __DIR__ . '/migrations/' => database_path('migrations')
-		        ], 'backstory-migrations');
+$this->publishes([
+__DIR__ . '/migrations/' => database_path('migrations')
+], 'backstory-migrations');
 
 
-		$this->loadMigrationsFrom(__DIR__ . '/migrations');
+$this->loadMigrationsFrom(__DIR__ . '/migrations');
 
-		if (!$this->app->runningInConsole()) :
-				            include_once __DIR__ . '/Helpers/helper.php';
-		endif;
+if (!$this->app->runningInConsole()) :
+    include_once __DIR__ . '/Helpers/helper.php';
+endif;
 
-		$this->loadProviders();
-		$this->loadAliases();
+$this->loadProviders();
+$this->loadAliases();
 
 
 
-	}
+}
 
 
 
-	/**
-	* Register any package services.
-		     *
-		     * @return void
-		     */
-		    public function register()
-		    {
+/**
+* Register any package services.
+*
+* @return void
+*/
+public function register()
+{
 
-		$this->mergeConfigFrom(
-				            __DIR__ . '/config/config.php', 'backstory'
-				        );
+$this->mergeConfigFrom(
+    __DIR__ . '/config/config.php', 'backstory'
+);
 
-		$this->app->bind(
-				        'Backstory', function () {
-			return new Backstory();
-		}
-		);
+$this->app->bind(
+'Backstory', function () {
+return new Backstory();
+}
+);
 
-		$this->registerFactoriesPath(__DIR__.'/factories');
+$this->registerFactoriesPath(__DIR__.'/factories');
 
-	}
+}
 
 
 
 
-	/**
-	* Register factories.
-		    *
-		    * @param  string  $path
-		    * @return void
-		    */
-		    protected function registerFactoriesPath($path)
-		    {
-		$this->app->make(Factory::class)->load($path);
-	}
+/**
+* Register factories.
+*
+* @param  string  $path
+* @return void
+*/
+protected function registerFactoriesPath($path)
+{
+$this->app->make(Factory::class)->load($path);
+}
 
 
-	public function loadProviders()
-		        {
+public function loadProviders()
+{
 
 
 
-		/*
-		* load service providers
-				             */
+/*
+* load service providers
+        */
 
-		// 		$this->app->register(\Collective\Html\HtmlServiceProvider::class);
-        $this->app->register(HtmlServiceProvider::class);
+// 		$this->app->register(\Collective\Html\HtmlServiceProvider::class);
+$this->app->register(HtmlServiceProvider::class);
 
 
 
 
 
-		/*
-		* Dev resources
-				             */
+/*
+* Dev resources
+        */
 
-		if ($this->app->environment() !== "production") {
+if ($this->app->environment() !== "production") {
 
 
 
 
-		}
+}
 
 
-	}
+}
 
-	public function loadAliases() {
+public function loadAliases() {
 
 
 
 
-		/*
-		* Load aliases / facades
-				             */
-				            $aliases = \Illuminate\Foundation\AliasLoader::getInstance();
+/*
+* Load aliases / facades
+        */
+    $aliases = \Illuminate\Foundation\AliasLoader::getInstance();
 
-		// 		$aliases->alias("Html", \Collective\Html\HtmlFacade::class);
+// 		$aliases->alias("Html", \Collective\Html\HtmlFacade::class);
 
 
 
 
-		/*
-		* Dev resources
-				           */
+/*
+* Dev resources
+    */
 
-		if ($this->app->environment() !== "production") {
+if ($this->app->environment() !== "production") {
 
-			//$			aliases->alias('Debugbar', 'Barryvdh\Debugbar\Facade');
+//$			aliases->alias('Debugbar', 'Barryvdh\Debugbar\Facade');
 
-		}
+}
 
 
-	}
+}
 
 
 }
