7f19c429f4a050d7abbbd0f09c410ea7dbe7b78d
Shawn Sandy <shawnsandy04@gmail.com>
2017-12-07 22:40:54 -0500

    Features : adds ability browse and insert images in content

--- src/resources/views/partials/manage-images.blade.php
+++ src/resources/views/partials/manage-images.blade.php
@@ -1,17 +1,46 @@
-
-@component('backstory::components.modals', ["modal_name" => "image-modal"])
-@slot('title')
-Insert Images
-@endslot
-<div class="columns is-multiline">
-@foreach(backstory()->getImgs() as $item)
-    <div class="column is-4">
-    <img src="{{ backstory()->imgFly($item) }}" alt=""></div>
-@endforeach
+<div class="modal image-modal">
+  <div class="modal-background"></div>
+  <div class="modal-card">
+    <header class="modal-card-head">
+      <div class="container is-fluid content image-fields">
+      <p class="subtitle is-6 is-uppercase">Insert Images</p>
 </div>
-@slot('footer')
-    <p class="">
-    <button type="button" class="button is-success close-box">Insert Image</button>
+    </header>
+    <section class="modal-card-body">
+      <div class="container is-fluid">
+
+    <div class="columns is-multiline">
+        @foreach(backstory()->getImgs() as $item)
+            <div class="column is-3">
+                <img class="select-image" data-img="{{ backstory()->imgFly($item, 'small')  }}" src="{{ backstory()->imgFly($item, 'small') }}" alt="">
+                <p class="has-text-centered">
+                <button class="button is-small is-info">Insert</button>
+                <a href="#" class="button is-small is-info is-outlined insert-images-button close-box" data-img="{{ url(backstory()->imgFly($item, 'small'))  }}">Small</a>
+                <a href="#" class="button is-small is-info is-outlined insert-images-button close-box" data-img="{{ url(backstory()->imgFly($item, 'medium'))  }}">Med</a>
+                <a href="#" class="button is-small is-info is-outlined insert-images-button close-box" data-img="{{ url(backstory()->imgFly($item, 'large'))  }}">Large</a>
+                </p>
+            </div>
+        @endforeach
+    </div>
+
+</div>
+    </section>
+    <footer class="modal-card-foot">
+    <div class="content">
+    <p class="has-text-right">
+        <button type="button" class="button is-info is-outlined close-box">
+        <span class="icon"><i class="fa fa-close"></i> </span><span>Close</span></button>
     </p>
-@endslot
-@endcomponent
+    </div>
+    </footer>
+  </div>
+  <div>
+  <button type="button" class="modal-close is-large" aria-label="close"></button>
+  </div>
+</div>
+@push('scripts')
+<script>
+
+
+</script>
+@endpush
