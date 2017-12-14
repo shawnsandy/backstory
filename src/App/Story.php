484a2dbc61f68a826501ebf629eb6edc21e90e8f
Shawn Sandy <shawnsandy04@gmail.com>
2017-12-14 18:42:10 -0500

    Update : refactor the query on the category route
    - add a hasCategory scope to the model
    - apply the scope to query on the category route

--- src/App/Story.php
+++ src/App/Story.php
@@ -8,122 +8,142 @@ use Illuminate\Database\Eloquent\Model;
 class Story extends Model
 {
 
-    protected $fillable = [
-        'title',
-        'content',
-        'content_plain',
-        'introduction',
-        'cover_photo',
-        'status',
-        'type',
-        'tags',
-    ];
+	protected $fillable = [
+	        'title',
+	        'content',
+	        'content_plain',
+	        'introduction',
+	        'cover_photo',
+	        'status',
+	        'type',
+	        'tags',
+	    ];
 
-    public function author()
-    {
-        return $this->belongsTo(User::class, 'user_id');
-    }
+	public function author()
+	    {
+		return $this->belongsTo(User::class, 'user_id');
+	}
 
-    public function categories()
-    {
-        return $this->belongsToMany(StoryCategory::class);
-    }
+	public function categories()
+	    {
+		return $this->belongsToMany(StoryCategory::class);
+	}
 
-    public function attachments()
-    {
-        return $this->belongsToMany(StoryAttachment::class);
-    }
+	public function attachments()
+	    {
+		return $this->belongsToMany(StoryAttachment::class);
+	}
 
-    public function options()
-    {
-        return $this->hasMany(StoryOption::class);
-    }
+	public function options()
+	    {
+		return $this->hasMany(StoryOption::class);
+	}
 
-    /**
-     * Find category by id
-     *
-     *
-     * @param mixed $query
-     * @param string $tag
-     * <code> Story::getTag('red')->get();</code>
-     * @return void
-     */
-    public function scopeGetTag($query, $tag = null)
-    {
-        if(is_null($tag))
-        return null;
 
-        return $query->where('tags', 'like', "%{$tag}%");
-    }
+	/**
+	* Find category by id
+	     *
+	     *
+	     * @param mixed $query
+	     * @param string $tag
+	     * <code> Story::getTag('red')->get();
+	</code>
+	     * @return void
+	     */
+	    public function scopeGetTag($query, $tag = null)
+	    {
+		if(is_null($tag))
+		        return null;
 
-    /**
-     * Find category by Id
-     *
-     * @param mixed $query
-     * @param string $id
-     * <code>Story::categoryId(1)->get();</code>
-     *
-     * @return void
-     */
-    public function scopeCategoryId($query, $id = null)
-    {
-        if(!is_null($id)) {
+		return $query->where('tags', 'like', "%{$tag}%");
+	}
 
-            return $query->with(['categories' => function($query) use ($id){
-                $query->where('id', $id);
-            }]);
 
-        }
-        return null;
+	/**
+	* Find category by Id
+	     *
+	     * @param mixed $query
+	     * @param string $id
+	     * <code>Story::categoryId(1)->get();
+	</code>
+	     *
+	     * @return void
+	     */
+	    public function scopeCategoryId($query, $id = null)
+	    {
+		if(!is_null($id)) {
 
-    }
+			return $query->with(['categories' => function($query) use ($id){
+				$query->where('id', $id);
+			}
+			]);
 
-    /**
-     * Find category by name
-     *
-     * @param mixed $query
-     * @param string $name
-     * <code> Story::categoryName('general')->get();</code>
-     *
-     * @return void
-     */
-    public function scopeCategoryName($query, $name = null)
-    {
-        if(!is_null($name)) {
+		}
+		return null;
 
-            return $query->with(['categories' => function($query) use ($name){
-                $query->where('name', $name);
-            }]);
+	}
 
-        }
-        return null;
+	public function scopeHasCategory($query, $id)
+	{
+		return $query->with('categories', 'author')->whereHas('categories', function($qry) use($id) {
 
-    }
+			$qry->where('id', $id);
 
-    public function getCategoryAttribute(){
+		}
+		);
+	}
 
-       return $this->categories->pluck(['id']);
 
-    }
 
-    public function getByAttribute()
-    {
-        return $this->author->name;
-    }
 
-    public function getCoverThumbnailAttribute()
-    {
-    //    return "/imgfly/public/".$this->cover_photo.config("backstory.img.thumbs");
-        return backstory()->imgFly($this->cover_photo);
-    }
+	/**
+	* Find category by name
+	     *
+	     * @param mixed $query
+	     * @param string $name
+	     * <code> Story::categoryName('general')->get();
+	</code>
+	     *
+	     * @return void
+	     */
+	    public function scopeCategoryName($query, $name = null)
+	    {
+		if(!is_null($name)) {
 
-    public function getCoverAttribute()
-    {
+			return $query->with(['categories' => function($query) use ($name){
+				$query->where('name', $name);
+			}
+			]);
 
-        if($this->cover_photo)
-        return backstory()->imgFly($this->cover_photo, 'cover');
+		}
+		return null;
 
-    }
+	}
+
+	public function getCategoryAttribute(){
+
+		return $this->categories->pluck(['id']);
+
+	}
+
+	public function getByAttribute()
+	    {
+		return $this->author->name;
+	}
+
+	public function getCoverThumbnailAttribute()
+	    {
+		// 		return "/imgfly/public/".$this->cover_photo.config("backstory.img.thumbs");
+		return backstory()->imgFly($this->cover_photo);
+	}
+
+	public function getCoverAttribute()
+	    {
+
+		if($this->cover_photo)
+		        return backstory()->imgFly($this->cover_photo, 'cover');
+
+	}
 
 
 }
--- src/routes.php
+++ src/routes.php
@@ -13,17 +13,12 @@ Route::get("stories", function() {
     $stories = backstory()->latestStories();;
 
  return view("backstory::index", compact('stories'));
-} );
+});
 
 Route::get('/story/category/{id}', function($id){
 
-    $stories = Story::with('categories', 'author')->whereHas('categories', function($qry) use($id) {
-
-        $qry->where('id', $id);
-
-    })->paginate(config('backstory.stories_per_page'));
-
-
+    $stories = Story::hasCategory($id)
+    ->paginate(config('backstory.stories_per_page'));
     return view("backstory::index", compact('stories'));
 
 });
