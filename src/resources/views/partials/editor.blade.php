8e31f87bf1842d10c764e58854775242a554a9a8
Shawn Sandy <shawnsandy04@gmail.com>
2017-12-17 17:28:53 -0500

    update
    Layout adds the editor bar to page

--- src/resources/views/partials/editor.blade.php
+++ src/resources/views/partials/editor.blade.php
@@ -1,17 +1,15 @@
 <div class="field">
     <div class="control">
-    <div class="editor-bar is-uppercase">
-    <div class="columns">
-        <div class="column is-3 has-text-light">
-            {{ Backstory::label('Add your story', 'content') }} {{--
-            <span class="tag is-dark"> :: </span> --}}
+        <div class="editor-bar is-uppercase">
+            <div class="columns">
+                <div class="column is-3 has-text-light">
+                    {{ Backstory::label('Write your story', 'content') }}
+                </div>
+                <div class="column has-text-right">
+                    @include("backstory::partials.edit-bar")
+                </div>
+            </div>
         </div>
-        <div class="column has-text-right">
-            @include("backstory::partials.edit-bar")
-        </div>
-    </div>
-</div>
-
         {{ Backstory::content() }}
     </div>
 </div>
