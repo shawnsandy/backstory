fea69aaa00b274ce7819c2e12d6884cc9dc22b49
Shawn Sandy <shawnsandy04@gmail.com>
2017-12-17 05:59:35 -0500

    Layout : adds edit bar markup
    Move the edit bar markup from its own view to the editor view to simplify the views

--- src/resources/views/partials/editor.blade.php
+++ src/resources/views/partials/editor.blade.php
@@ -1,6 +1,17 @@
 <div class="field">
     <div class="control">
-        @include("backstory::partials.edit-bar")
+    <div class="editor-bar is-uppercase">
+    <div class="columns">
+        <div class="column is-3 has-text-light">
+            {{ Backstory::label('Add your story', 'content') }} {{--
+            <span class="tag is-dark"> :: </span> --}}
+        </div>
+        <div class="column has-text-right">
+            @include("backstory::partials.edit-bar")
+        </div>
+    </div>
+</div>
+
         {{ Backstory::content() }}
     </div>
 </div>
